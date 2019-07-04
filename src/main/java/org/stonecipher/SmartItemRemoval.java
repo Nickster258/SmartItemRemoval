80b74475db828d0a74d929e3a4fb07f6cdae71a1
BuildTools <nickster258@gmail.com>
2019-07-04 19:21:58 -0400

    Added toggle option to retain dropped items

--- src/main/java/org/stonecipher/SmartItemRemoval.java
+++ src/main/java/org/stonecipher/SmartItemRemoval.java
@@ -1,9 +1,10 @@
 package org.stonecipher;
 
-import org.bukkit.GameRule;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ItemSpawnEvent;
@@ -15,6 +16,7 @@ import java.util.ArrayList;
 
 public class SmartItemRemoval extends JavaPlugin implements Listener {
 
+    private ArrayList<Player> keepDropPlayers = new ArrayList<Player>();
     private FileConfiguration config = getConfig();
     private ArrayList<Item> items = new ArrayList<Item>();
     private int limit = 0;
@@ -22,9 +24,27 @@ public class SmartItemRemoval extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
+        setupConfig();
         limit = config.getInt("item_count_limit");
         getLogger().info("Set up max item count of " + limit);
-        setupConfig();
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            return true;
+        }
+
+        if(cmd.getName().equalsIgnoreCase("keepdrops")) {
+            if(keepDropPlayers.contains(sender)) {
+                keepDropPlayers.remove(sender);
+                sendMessage(sender, "You have opted out of retaining dropped items.");
+            } else {
+                keepDropPlayers.add((Player) sender);
+                sendMessage(sender, "You are opted into retaining dropped items.");
+            }
+        }
+        return true;
     }
 
     @EventHandler
@@ -44,7 +64,11 @@ public class SmartItemRemoval extends JavaPlugin implements Listener {
     @EventHandler
     private void onPlayerItemDropEvent(PlayerDropItemEvent e) {
         if (limit == 0) {
-            e.setCancelled(true);
+            if (keepDropPlayers.contains(e.getPlayer())) {
+                e.setCancelled(true);
+            } else {
+                e.getItemDrop().remove();
+            }
             return;
         }
         if (items.size() > limit) {
@@ -53,6 +77,10 @@ public class SmartItemRemoval extends JavaPlugin implements Listener {
             items.remove(0);
         }
         items.add(e.getItemDrop());
+    }
+
+    public void sendMessage(CommandSender sender, String message) {
+        sender.sendMessage("§7" + message);
     }
 
     private void setupConfig() {
